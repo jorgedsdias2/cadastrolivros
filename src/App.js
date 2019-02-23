45c679ffb7211b9749bdd956e8635d24de70e66a
Jorge Dias <jorgedsdias@gmail.com>
2019-02-22 22:28:33 -0300

    Subindo componentes customizados

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state = {lista: []};
+    this.state = {lista: [], nome: '', email: '', senha: ''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
   }
 
   componentDidMount() {
@@ -19,6 +25,37 @@ class App extends Component {
       }.bind(this)
     });
   }
+
+  enviaForm(evento) {
+    evento.preventDefault();
+    
+    $.ajax({
+      url:'http://cdc-react.herokuapp.com/api/autores',
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'POST',
+      data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
+      success: function(resposta) {
+        this.setState({lista:resposta});
+        console.log("Enviado com sucesso!");
+      }.bind(this),
+      error: function(resposta) {
+        console.log("Erro: Falha no envio!");
+      }
+    });
+  }
+
+  setNome(evento) {
+    this.setState({nome: evento.target.value});
+  }
+
+  setEmail(evento) {
+    this.setState({email: evento.target.value});    
+  }
+  
+  setSenha(evento) {
+    this.setState({senha: evento.target.value});    
+  }  
 
   render() {
     return (
@@ -44,23 +81,11 @@ class App extends Component {
                 </div>
                 <div className="content" id="content">
                   <div className="pure-form pure-form-aligned">
-                    <form className="pure-form pure-form-aligned">
-                      <div className="pure-control-group">
-                        <label htmlFor="nome">Nome</label> 
-                        <input id="nome" type="text" name="nome" value=""  />                  
-                      </div>
-                      <div className="pure-control-group">
-                        <label htmlFor="email">Email</label> 
-                        <input id="email" type="email" name="email" value=""  />                  
-                      </div>
-                      <div className="pure-control-group">
-                        <label htmlFor="senha">Senha</label> 
-                        <input id="senha" type="password" name="senha"  />                                      
-                      </div>
-                      <div className="pure-control-group">                                  
-                        <label></label> 
-                        <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                      </div>
+                    <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="POST">
+                      <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
+                      <InputCustomizado id="email" type="text" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
+                      <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
+                      <BotaoSubmitCustomizado label="Gravar"/>
                     </form>             
 
                   </div>  
